a0ad99d91c6b55fc6136cb3a46e0a733b3903f5b
ganotra <ganotra@adobe.com>
2021-10-05 18:30:53 +0530

    Fixing failing deploys

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,10 +10,6 @@
  * governing permissions and limitations under the License.
  */
 
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
-
 module.exports = {
   siteMetadata: {
     home: {
