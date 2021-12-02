b33817098d30f067b22a9512f2b85b42832390fb
vaibhav kumar <vaibhavk@adobe.com>
2021-12-02 22:15:42 +0530

    Added Autotag PDF beta operation init docs

--- gatsby-config.js
+++ gatsby-config.js
@@ -93,6 +93,10 @@ module.exports = {
             title: 'PDF Embed API',
             path: 'overview/pdf-embed-api/index.md'
           },
+          {
+            title: 'PDF Autotag API',
+            path: 'overview/pdf-autotag-api/beta/index.md'
+          },
         ]
       },
     ],
@@ -297,6 +301,36 @@ module.exports = {
           {
             title: 'Release notes',
             path: 'overview/pdf-embed-api/releasenotes.md'
+          },
+        ]
+      },
+      {
+        title: 'PDF Autotag API',
+        path: 'overview/pdf-autotag-api/beta/index.md',
+        pages: [
+          {
+            title: 'Overview',
+            path: 'overview/pdf-autotag-api/beta/index.md'
+          },
+          {
+            title: 'Quickstarts',
+            path: 'overview/pdf-autotag-api/beta/quickstarts.md'
+          },
+          {
+            title: 'How Tos',
+            path: 'overview/pdf-autotag-api/beta/howtos/howtos.md'
+          },
+          {
+            title: 'PDF Autotag API',
+            path: 'overview/pdf-autotag-api/beta/howtos/autotag-api.md'
+          },
+          {
+            title: 'Release Notes',
+            path: 'overview/pdf-autotag-api/beta/releasenotes.md'
+          },
+          {
+            title: 'PDF Services API Licensing',
+            path: 'overview/pdf-autotag-api/beta/dcservicelicensing.md'
           },
         ]
       },
