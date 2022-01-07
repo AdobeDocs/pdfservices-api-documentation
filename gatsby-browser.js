392b2eafe6e878f802a321171beed1b82f037eec
timkim <timk@adobe.com>
2022-01-07 15:08:57 -0800

    Format page url

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -51,15 +51,18 @@ export const onRouteUpdate = ({ location, prevLocation }) => {
 
     let pageName = location.pathname.replace('/','');
     pageName = location.pathname.replaceAll('/',':');
+
+    let pageUrl = location.pathname.replace('/','');
+    pageUrl = location.pathname.replaceAll('/',':');
     if(window.digitalData._set){
       window.digitalData._set('digitalData.page.pageInfo.pageName', pageName);
+      window.digitalData._set('digitalData.page.pageInfo.pageUrl', pageUrl);
       if(window._satellite){
         window._satellite.track('state', {
           digitalData: window.digitalData._snapshot()
         });  
       }
     }
-
 
     let getCredentialsButton = Array.from(document.querySelectorAll('a')).find(el => el.textContent === 'Get credentials');
     // production
