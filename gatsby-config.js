56014745ef677b35ddc5410b468c8c75d49bd2d2
ganotra <ganotra@adobe.com>
2022-01-11 23:36:08 +0530

    Fixing URL in Resources tab

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,7 +67,7 @@ module.exports = {
           path: 'https://community.adobe.com/t5/document-services-apis/bd-p/Document-Cloud-SDK?page=1&sort=latest_replies&filter=all'
         }, {
           title: 'Licensing',
-          path: 'document-services/docs/overview/pdf-services-api/dcserviceslicensing.md'
+          path: 'https://developer-stage.adobe.com/document-services/docs/overview/pdf-services-api/dcserviceslicensing?aio_internal'
         }]
       },
       {
