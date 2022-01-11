f34b649e052d111f85ded37de8edbb082ccbc819
ganotra <ganotra@adobe.com>
2022-01-11 22:43:45 +0530

    Fixing licensing page

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,7 +67,7 @@ module.exports = {
           path: 'https://community.adobe.com/t5/document-services-apis/bd-p/Document-Cloud-SDK?page=1&sort=latest_replies&filter=all'
         }, {
           title: 'Licensing',
-          path: 'overview/pdf-services-api/dcserviceslicensing.md'
+          path: '/docs/overview/pdf-services-api/dcserviceslicensing.md'
         }]
       },
       {
