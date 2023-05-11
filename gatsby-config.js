1dadec2df739fd803cf74e97e109269d71f325e4
Himanshu Jindal <himanshuj@adobe.com>
2023-05-11 14:39:35 +0530

    Removed node and dotnet sdk page section

--- gatsby-config.js
+++ gatsby-config.js
@@ -435,16 +435,8 @@ module.exports = {
                         path: 'overview/pdf-electronic-seal-api/quickstarts',
                         pages: [
                             {
-                                title:'Node.js',
-                                path: 'overview/pdf-electronic-seal-api/quickstarts/nodejs/index.md'
-                            },
-                            {
                                 title:'Java',
                                 path: 'overview/pdf-electronic-seal-api/quickstarts/java/index.md'
-                            },
-                            {
-                                title:'.NET',
-                                path: 'overview/pdf-electronic-seal-api/quickstarts/dotnet/index.md'
                             }
                         ]
 
