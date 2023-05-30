963cca85dc3c1a1bbdae6a0d364f4087e7d3cbce
louisachu <louisa@ensemble.com>
2023-05-29 17:23:05 -0700

    testing

--- static/redirections.js
+++ static/redirections.js
@@ -27,79 +27,79 @@
       window.location.host.indexOf("localhost") >= 0 &&
       window.location.pathname === "/overview/")
   ) {
-    switch (window.location.hash) {
-      case "#adobe-analytics":
-        redirectPath = prefixPath + `pdf-embed-api/howtodata/${window.location.search}#adobe-analytics`;
-        break;
-      case "#pdf-extract-api-for-extracting-pdf-structure-and-information":
-        redirectPath =
-          prefixPath +
-          `${window.location.search}#pdf-extract-api-for-extracting-pdf-structure-and-information`;
-        break;
-      case "#beta-feature-extract-text-and-tables-and-styling-info":
-        redirectPath =
-          prefixPath +
-          `pdf-extract-api/howtos/extract-api/${window.location.search}#beta-feature-extract-text-and-tables-and-styling-info`;
-        break;
-      case "#export-pdf":
-        redirectPath =
-          prefixPath + `pdf-services-api/howtos/export-pdf/${window.location.search}#export-a-pdf`;
-        break;
-      case "#extract-pdf":
-        redirectPath = prefixPath + `pdf-services-api/howtos/extract-pdf${window.location.search}`;
-        break;
-      case "#extract-text-and-tables":
-        redirectPath =
-          prefixPath +
-          `pdf-services-api/howtos/extract-pdf/${window.location.search}#extract-text-and-tables`;
-        break;
-      case "#extract-text-and-tables-and-character-bounding-boxes-w-renditions":
-        redirectPath =
-          prefixPath +
-          `pdf-services-api/howtos/extract-pdf/${window.location.search}#extract-text-and-tables-and-character-bounding-boxes-w-renditions`;
-        break;
-      case "#extract-text-and-tables-and-table-structure-as-csv-w-renditions":
-        redirectPath =
-          prefixPath +
-          `pdf-services-api/howtos/extract-pdf/${window.location.search}#extract-text-and-tables-and-table-structure-as-csv-w-renditions`;
-        break;
-      case "#extract-text-and-tables-w-renditions":
-        redirectPath =
-          prefixPath +
-          `pdf-services-api/howtos/extract-pdf/${window.location.search}#extract-text-and-tables-w-tables-renditions`;
-        break;
-      case "#extract-text-from-a-pdf":
-        redirectPath =
-          prefixPath +
-          `pdf-services-api/howtos/extract-pdf/${window.location.search}#extract-text-from-a-pdf`;
-        break;
-      case "#ocr-pdf":
-        redirectPath = prefixPath + `pdf-services-api/howtos/ocr-pdf`;
-        break;
-      case "#protect-pdf":
-        redirectPath = prefixPath + `pdf-services-api/howtos/protect-pdf${window.location.search}`;
-        break;
-      case "#protect-pdfs-with-user-password":
-        redirectPath =
-          prefixPath +
-          `pdf-services-api/howtos/protect-pdf/${window.location.search}#protect-pdfs-with-owner-password`;
-        break;
-      case "#split-pdf":
-        redirectPath = prefixPath + `pdf-services-api/howtos/split-pdf${window.location.search}`;
-        break;
-      case "#java":
-        redirectPath = prefixPath + `pdf-services-api/${window.location.search}#java`;
-        break;
-      case "#node-js":
-        redirectPath = prefixPath + `pdf-services-api/${window.location.search}#nodejs`;
-        break;
-      case "#python":
-        redirectPath = prefixPath + `pdf-services-api/${window.location.search}#python`;
-        break;
-      case "#sdk":
-        redirectPath = prefixPath + `pdf-services-api/${window.location.search}#sdk`;
-        break;
-    }
+    // switch (window.location.hash) {
+    //   case "#adobe-analytics":
+    //     redirectPath = prefixPath + `pdf-embed-api/howtodata/${window.location.search}#adobe-analytics`;
+    //     break;
+    //   case "#pdf-extract-api-for-extracting-pdf-structure-and-information":
+    //     redirectPath =
+    //       prefixPath +
+    //       `${window.location.search}#pdf-extract-api-for-extracting-pdf-structure-and-information`;
+    //     break;
+    //   case "#beta-feature-extract-text-and-tables-and-styling-info":
+    //     redirectPath =
+    //       prefixPath +
+    //       `pdf-extract-api/howtos/extract-api/${window.location.search}#beta-feature-extract-text-and-tables-and-styling-info`;
+    //     break;
+    //   case "#export-pdf":
+    //     redirectPath =
+    //       prefixPath + `pdf-services-api/howtos/export-pdf/${window.location.search}#export-a-pdf`;
+    //     break;
+    //   case "#extract-pdf":
+    //     redirectPath = prefixPath + `pdf-services-api/howtos/extract-pdf${window.location.search}`;
+    //     break;
+    //   case "#extract-text-and-tables":
+    //     redirectPath =
+    //       prefixPath +
+    //       `pdf-services-api/howtos/extract-pdf/${window.location.search}#extract-text-and-tables`;
+    //     break;
+    //   case "#extract-text-and-tables-and-character-bounding-boxes-w-renditions":
+    //     redirectPath =
+    //       prefixPath +
+    //       `pdf-services-api/howtos/extract-pdf/${window.location.search}#extract-text-and-tables-and-character-bounding-boxes-w-renditions`;
+    //     break;
+    //   case "#extract-text-and-tables-and-table-structure-as-csv-w-renditions":
+    //     redirectPath =
+    //       prefixPath +
+    //       `pdf-services-api/howtos/extract-pdf/${window.location.search}#extract-text-and-tables-and-table-structure-as-csv-w-renditions`;
+    //     break;
+    //   case "#extract-text-and-tables-w-renditions":
+    //     redirectPath =
+    //       prefixPath +
+    //       `pdf-services-api/howtos/extract-pdf/${window.location.search}#extract-text-and-tables-w-tables-renditions`;
+    //     break;
+    //   case "#extract-text-from-a-pdf":
+    //     redirectPath =
+    //       prefixPath +
+    //       `pdf-services-api/howtos/extract-pdf/${window.location.search}#extract-text-from-a-pdf`;
+    //     break;
+    //   case "#ocr-pdf":
+    //     redirectPath = prefixPath + `pdf-services-api/howtos/ocr-pdf`;
+    //     break;
+    //   case "#protect-pdf":
+    //     redirectPath = prefixPath + `pdf-services-api/howtos/protect-pdf${window.location.search}`;
+    //     break;
+    //   case "#protect-pdfs-with-user-password":
+    //     redirectPath =
+    //       prefixPath +
+    //       `pdf-services-api/howtos/protect-pdf/${window.location.search}#protect-pdfs-with-owner-password`;
+    //     break;
+    //   case "#split-pdf":
+    //     redirectPath = prefixPath + `pdf-services-api/howtos/split-pdf${window.location.search}`;
+    //     break;
+    //   case "#java":
+    //     redirectPath = prefixPath + `pdf-services-api/${window.location.search}#java`;
+    //     break;
+    //   case "#node-js":
+    //     redirectPath = prefixPath + `pdf-services-api/${window.location.search}#nodejs`;
+    //     break;
+    //   case "#python":
+    //     redirectPath = prefixPath + `pdf-services-api/${window.location.search}#python`;
+    //     break;
+    //   case "#sdk":
+    //     redirectPath = prefixPath + `pdf-services-api/${window.location.search}#sdk`;
+    //     break;
+    // }
 
     window.location.href =
       decodeURIComponent("http://" + window.location.host + "/" + redirectPath);
